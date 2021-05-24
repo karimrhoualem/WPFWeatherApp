1e094c756ed345b525089b93e01ba1747a7ca5d6
Karim Rhoualem <karim.rhoualem@gmail.com>
2021-05-24 14:43:24 -0400

    Added the Model and View Model that makes AccuWeather API GET requests.

---
+++ WPFWeatherApp/Model/City.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/// <summary>
+/// Used https://developer.accuweather.com/accuweather-locations-api/apis/get/locations/v1/cities/autocomplete
+/// and http://jsonutils.com/
+/// to generate this class.
+/// </summary>
+namespace WPFWeatherApp.Model
+{
+    public class Area
+    {
+        public string ID { get; set; }
+        public string LocalizedName { get; set; }
+    }
+
+    public class City
+    {
+        public int Version { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public int Rank { get; set; }
+        public string LocalizedName { get; set; }
+        public Area Country { get; set; }
+        public Area AdministrativeArea { get; set; }
+    }
+}
